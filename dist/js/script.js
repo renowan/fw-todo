9c3097f1ea5c0b27b4d21991366a7f55a2d923f3
sekiya <sekiya@gnavi.co.jp>
2017-05-23 10:34:27 +0900

    jquery版 DOM

--- dist/js/script.js
+++ dist/js/script.js
@@ -11,17 +11,26 @@
     init: function () {
       let that = this;
       
+      let config = {
+        component: '.js-todo',
+        delete: '.js-btnDel',
+        add: '.js-btnAdd'
+      };
+      
       // event
-      that.event ();
+      that.event (config);
     },
     // event
-    event: function () {
+    event: function (config) {
       let that = this;
 
       console.log('event');
       
       // delete
-      $('.js-todo').on('click', '.js-btnDel', that.delete);
+      $(config.component).on('click', config.delete, that.delete);
+
+      // add
+      $(config.component).on('click', config.add, that.add);
     },
     delete: function (e) {
       e.preventDefault();
@@ -31,10 +40,19 @@
       
       $(this)
         .closest('li')
+        .removeClass('is_show')
         .addClass('is_deleted')
-        .on('animationend webkitAnimationEnd oAnimationEnd mozAnimationEnd',function(){
-          console.log('webkitAnimationEnd');
+        .on('animationend webkitAnimationEnd oAnimationEnd mozAnimationEnd', function(){
+          console.log($(this));
+          $(this).addClass('is_hide');
         });
+    },
+    add: function (e) {
+      e.preventDefault();
+      let that = this;
+      
+      let html = '<li class="is_show"><p>ふがふがふ</p><p class="button-delete js-btnDel"></p></li>';
+      $('.js-todo ul').prepend(html);
     }
     
   };
