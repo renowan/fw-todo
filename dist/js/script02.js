77d9c8111198b60a53a85e267bebe0b2c7874f20
sekiya <sekiya@gnavi.co.jp>
2017-07-10 18:31:56 +0900

    vue

--- dist/js/script02.js
+++ dist/js/script02.js
@@ -1,19 +1,37 @@
 {
 
   Vue.component ('todo', {
-    template: '#js-template-todo'
+    template: '#js-template-todo',
+    computed: {
+      leng () {
+        return this.list.length
+      }
+    },
+    data () {
+      return {
+        list: [
+          {'name': 'hoge'},
+          {'name': 'huga'},
+          {'name': 'piyo'},
+          {'name': 'ponyo'}
+        ]
+      }
+    },
+    created () {
+
+    },
+    methods: {
+      add (e) {
+        this.list.push(e)
+      },
+      remove (e) {
+        this.list.push(e)
+      }
+    }
   });
 
   new Vue({
-    el: '.js-todo',
-    components: {
-
-    },
-    computed: {
-      leng () {
-        return 3
-      }
-    }
+    el: '.js-todo'
   });
 
 
