2c32e61be6d924e74b05ef293e35a9a29f8ba983
sekiya <sekiya@gnavi.co.jp>
2017-08-07 16:21:54 +0900

    vue

--- dist/js/script02.js
+++ dist/js/script02.js
@@ -13,8 +13,10 @@
         list: JSON.parse(localStorage.getItem('todo')) || ['　'],
         isEdited: false,
         item: '',
-        isShow: true,
-        isHide: false
+        todo_isShow: true,
+        todo_isHide: false,
+        add_isShow: false,
+        add_isHide: true
       }
     },
     created () {
@@ -22,9 +24,44 @@
     },
     methods: {
       add (e) {
-        this.list.push(e)
+        const targetAdd = document.getElementById('js-item-add');
+        targetAdd.getElementsByTagName('input')[0].focus();
+        this.add_isShow = !this.add_isShow;
+        this.add_isHide = !this.add_isHide;
+      },
+      addSet () {
+        console.log('addSet');
+
+        const targetAdd = document.getElementById('js-item-add');
+        let valAdd = targetAdd.getElementsByTagName('input')[0].value;
+        
+        if (valAdd === '') {return}
+
+        let listArr = [];
+        let data = {
+          title: valAdd
+        };
+        listArr.push(data);
+        
+        const target = document.getElementById('js-item-todo');
+        let elms = target.getElementsByTagName('li');
+        for (let i = 0, leng = elms.length; i < leng; i++) {
+          elms[i].classList.remove('is_edited');
+          let val = elms[i].getElementsByTagName('input')[0].value;
+          elms[i].getElementsByClassName('item-text').innerHTML = val;
+          if (val === '') {return}
+          data = {
+            title: val
+          };
+          listArr.push(data);
+        }
+        localStorage.setItem('todo', JSON.stringify(listArr));
+
+        this.add_isShow = !this.add_isShow;
+        this.add_isHide = !this.add_isHide;
       },
       remove (item, list) {
+        if (!confirm('削除しますか？')) {return}
         let idx = list.indexOf(item);
         let arr = JSON.parse(localStorage.getItem('todo'));
         arr.splice(idx, 1);
@@ -41,13 +78,23 @@
         }
         
         elm.classList.add('is_edited');
+        elm.getElementsByTagName('input')[0].focus();
       },
       set (item, list) {
         console.log('set');
-
+        let elms = document.getElementById('js-item-todo').getElementsByTagName('li');
+        let listArr = [];
         for (let i = 0, leng = elms.length; i < leng; i++) {
           elms[i].classList.remove('is_edited');
+          let val = elms[i].getElementsByTagName('input')[0].value;
+          elms[i].getElementsByClassName('item-text').innerHTML = val;
+          if (val === '') {return}
+          let data = {
+            title: val
+          };
+          listArr.push(data);
         }
+        localStorage.setItem('todo', JSON.stringify(listArr));
       }
     }
   });
